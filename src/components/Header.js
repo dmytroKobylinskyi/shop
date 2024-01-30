c490773625803e6de4dce5226b9121a06ad644c0
dmytroKobylinskyi <92432427+dmytroKobylinskyi@users.noreply.github.com>
2024-01-30 22:57:07 +0200

    shopping bag + the possibility of adding goods to the bag

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { FaShoppingCart } from "react-icons/fa"
+import Order from './Order';
 
-export default function Header() {
+const showOrders = (props)=>{
+  return(<div>
+    {props.orders.map(el=>(
+      <Order key={el.id} item={el} />
+    ))}
+  </div>)
+}
+
+const showNothing =  ()=>{
+  return (<div className='empty'>
+    <h2>Корзина пуста</h2>
+  </div>)
+}
+export default function Header(props) {
+  let[cartOpen,setCartOpen] = useState(false);
   return (
     <header>
         <div>
@@ -10,6 +26,14 @@ export default function Header() {
                 <li>Контакти</li>
                 <li>Кабінет</li>
             </ul>
+            <FaShoppingCart onClick={()=>setCartOpen(cartOpen=!cartOpen)}  className={`shop-cart-button ${cartOpen && 'active'}`}/>
+           
+           {cartOpen && (
+                <div className='shop-cart'>
+                  {props.orders.length > 0 ?
+                    showOrders(props) : showNothing()}
+                </div>
+            )}
         </div>
         <div className='presentation'></div>
     </header>
